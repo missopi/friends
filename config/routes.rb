736a96605c87803b7609b6f4af76be323284f055
missopi <missopi@me.com>
2024-01-26 14:47:53 +0000

    Make friends list the main home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friends
   # get 'home/index'
-  root 'home#index'
+  # root 'home#index'
+  root 'friends#index'
   get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
